46354751575966dde13da8f9e5c898e43bff97a9
markrissmiller <mprissmi@gmail.com>
2021-05-21 02:07:14 -0400

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux'
+import useStyles from './styles'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { getPosts } from './actions/posts'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+
+
+
+
+
+
+const  App = () =>{
+    const classes = useStyles();
+    const dispatch = useDispatch();
+    const [currentId, setCurrentId ] = useState(null)
+
+    const [quote, setQuote] = useState([])
+
+    const quoteApiCall = async () => {
+        try {
+            const res = await fetch('https://type.fit/api/quotes')
+            const data = await res.json()
+           
+            setQuote(data)
+            console.log(data)
+        } catch (error) {
+            console.log(error.message)
+        }
+        
+    }
+
+        
+    const selectedQuote = quote[Math.floor(Math.random() * quote.length)]
+        
+    
+    useEffect(() => {
+        dispatch(getPosts())
+        quoteApiCall()
+        
+    },[currentId, dispatch])
+   console.log(quote)
+    return(
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant='h2' align="center">SNAPSHOT</Typography> <br/>
+                <Typography >{selectedQuote ? `${selectedQuote.text} -${selectedQuote.author}` : ''}</Typography>
+            </AppBar>
+            <Grow in> 
+                <Container>
+                    <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts  setCurrentId={setCurrentId}/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Post from './Post/Post'
+import useStyles from './styles'
+import { useSelector } from 'react-redux'
+import { Grid, CircularProgress } from '@material-ui/core'
+
+export default function Posts({ setCurrentId }){
+    const posts = useSelector((state) => state.posts)
+    const classes = useStyles();
+
+    console.log(posts)
+    return(
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems='stretch' spacing={3}>
+                {posts.map((post) => (
+                    <Grid item key={post.id} xs={12} sm={6} > 
+                        <Post post={post}  setCurrentId={setCurrentId}/>
+                    </Grid>
+                ))}
+            </Grid>
+        )
+    )
+}
